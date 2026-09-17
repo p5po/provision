fa13d3ec09534392090b0e480876c285740cd4d3
p5po <abdullahBinTaleb7@gmail.com>
2026-09-17 11:40:25 +0300

    Use environment variable for Resend API key

--- api/auth/send.js
+++ api/auth/send.js
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 
-const resend = new Resend('re_3Pd8MyuD_MVzryqVjXqZuCnc7qunvw2H7');
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
